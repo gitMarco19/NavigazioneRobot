c05153de3c9106892960f7ab8e3e5306b0eecad6
marco <marcoscanu2019@live.com>
2020-06-16 17:33:26 +0200

    Added the file TipoCella-java which identifies all kind of cells into the maze

---
+++ src/TipoCella.java
@@ -0,0 +1,11 @@
+/**
+ * L'<i>enum</i> {@code TipoCella} indica gli stati
+ * in cui può trovarsi una {@code Cella} del <i>mondo artificiale</i>.
+ * 
+ * @author Marco Scanu
+ */
+public enum TipoCella {
+	VISITATA,
+	NON_VISITATA,
+	BLOCCATA;
+}
